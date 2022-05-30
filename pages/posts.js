c1eb080390802d0944748af09d7b1a999d3d6c1f
terrnit <terrnit.megaman@gmail.com>
2022-05-30 21:03:45 +0500

    small fixes

--- pages/posts.js
+++ pages/posts.js
@@ -1,4 +1,4 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
@@ -45,10 +45,11 @@ const Posts = () => (
       </Section>
 
       <Section delay={0.3}>
-        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <Divider my={6} />
           <Heading as="h3" fontSize={20} mb={4}>
             Blog Posts
           </Heading>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
             title="How I’ve Attracted The First 500 Paid Users For My SaaS That Costs $5/mo"
             thumbnail={thumb500PaidUsers}
